7fd62c1620fa088b411edaeb8a9b909e064cf219
Charlie Kushelevsky <ckushelevsky@ucsd.edu>
2024-09-29 20:31:51 -0700

    feat: updated main function

--- src/main.cpp
+++ src/main.cpp
@@ -10,11 +10,8 @@
 int main(int argc, char const* argv[])
 {
     std::string input = "(-3 (a + b^2) / sin(x)) * (-3x^yzcot(m))";
-    std::unique_ptr<Tokenizer> parser;
-    std::unique_ptr<ShuntingYard> converter;
-
-    parser = std::make_unique<Tokenizer>(input);
-    converter = std::make_unique<ShuntingYard>(parser->tokenize());
+    Tokenizer parser(input);
+    ShuntingYard converter(parser.tokenize());
     
     
     std::cout << "Hello World!\n";
