ffa3c02b8c0a1d56a4db91c8f5827495189f8c30
Charlie Kushelevsky <ckushelevsky@ucsd.edu>
2024-10-11 13:06:07 -0700

    feat: created base logic for derivative calculator

--- src/derivative.cpp
+++ src/derivative.cpp
@@ -3,10 +3,84 @@
 #include "token.hpp"
 #include "tokenizer.hpp"
 #include "postfix.hpp"
+#include "lookup.hpp"
+#include "calculator.hpp"
 
-Derivative::Derivative(std::string input)
+#include <stdexcept>
+#include <cmath>
+
+Derivative::Derivative(std::string input, std::string wrt)
 {
     Tokenizer parser(input);
+    Tokenizer diffVarParser(wrt);
+    auto diffVarParsed = parser.tokenize();
+    if (diffVarParsed.size() != 1)
+    {
+        std::string errMsg = "Invalid differentiating variable, ";
+        errMsg += "must be one variable, but parsed output has size ";
+        errMsg += std::to_string(diffVarParsed.size()) + ": " +
+            diffVarParsed.toString() + "\n";
+        std::runtime_error(errMsg.c_str());
+    }
+    this->diffVar = std::dynamic_pointer_cast<Variable>(diffVarParsed[0]);
+    if (diffVar->getType() != TokenType::VARIABLE)
+    {
+        std::string errMsg = "Invalid differentiating variable, ";
+        errMsg += "input must be a variable, but parsed token has type ";
+        errMsg += Lookup::getTokenType(diffVar->getType()) + ": " +
+            diffVar->getFullStr() + "\n";
+        std::runtime_error(errMsg.c_str());
+    }
+
     ShuntingYard converter(parser.tokenize());
     this->tree = ExpressionTree(converter.getPostfix());
+
 }
+
+
+void Derivative::solve()
+{
+    this->solve(this->tree.root);
+}
+
+std::shared_ptr<ExpressionNode> Derivative::solve(
+                nodePtr node)
+{
+    if (!node->hasVariable(this->diffVar))
+    {
+        node->removeLeftChild();
+        node->removeRightChild();
+        node->setDerivative(std::make_shared<ExpressionNode>(
+                                std::make_shared<Number>("0",0)));
+    }
+
+    return 0;
+}
+
+void Derivative::checkChildren(nodePtr node)
+{
+    if (!node->getLeft())
+    {
+        std::string errMsg = "The node " + node->getToken()->getFullStr() +
+            "has no left child\n";
+        std::runtime_error(errMsg.c_str());
+    }
+    if (!node->getRight())
+    {
+        std::string errMsg = "The node " + node->getToken()->getFullStr() +
+            "has no right child\n";
+        std::runtime_error(errMsg.c_str());
+    }
+    if (node->getRight()->getType() < node->getLeft()->getType() && 
+        node->getType() == TokenType::OPERATOR )
+    {
+        if (node->getToken()->isCommutative())
+        {
+            node->swapChildren();
+        }
+    }
+    
+}
+
+
+
--- src/derivative.hpp
+++ src/derivative.hpp
@@ -2,12 +2,27 @@
 #define __DERIVATIVE_HPP__
 
 #include "expression_tree.hpp"
+#include "expression_node.hpp"
+
+#include <memory>
 class Derivative
 {
+    typedef std::shared_ptr<ExpressionNode> nodePtr;
 private:
     ExpressionTree tree;
+    std::shared_ptr<Variable> diffVar;
 public:
-    Derivative(std::string input);
+    Derivative(std::string input, std::string wrt);
+    void solve();
+    nodePtr solve(nodePtr node);
+    nodePtr powerRule(nodePtr root);
+    nodePtr productRule(nodePtr root);
+    nodePtr quotientRule(nodePtr root);
+    nodePtr chainRule(nodePtr root);
+    nodePtr simplify(nodePtr node);
+    void checkChildren(nodePtr node);
+
+    
 };
 
 
