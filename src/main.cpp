9732db1c07d7deda4b856bd1fb0e97ca28a477a9
Charlie Kushelevsky <ckushelevsky@ucsd.edu>
2024-10-19 14:06:31 -0700

    feat: added string input to main function

--- src/main.cpp
+++ src/main.cpp
@@ -1,19 +1,48 @@
-#include "token.hpp"
-#include "tokenizer.hpp"
-#include "postfix.hpp"
+#include "derivative.hpp"
+#include "latex_converter.hpp"
+#include "token_queue.hpp"
+
 
 
 #include <iostream>
 #include <string>
 #include <memory>
 
+std::string getDerivative(std::string input)
+{
+    
+    Derivative out(input,"x");
+    return LaTeXConverter::convertToLaTeX(out.solve());
+}
 int main(int argc, char const* argv[])
 {
-    std::string input = "(-3 (a + b^2) / sin(x)) * (-3x^yzcot(m))";
-    Tokenizer parser(input);
-    ShuntingYard converter(parser.tokenize());
+   /*std::cout << argc << "\n";
+    if (argc == 1)
+    {
+        std::cout << "Input needed\n";
+        return 1;
+    }
+    std::string input = argv[1];*/
     
+    //"-2/5*ln(exp(x)-2*(2*x+3)/(5*x^2+x+4))+2/5*x"
+    std::string out = getDerivative("sin(cos(x))");
+    std::cout <<" \nDerivative: " << out << "\n";
+
+
+    auto testRoot = std::make_shared<ExpressionNode>(std::make_shared<Operator>("*"));
     
-    std::cout << "Hello World!\n";
+    auto func = std::make_shared<Function>("sin");
+    auto var = std::make_shared<Variable>("x");
+    auto queue = std::make_shared<TokenQueue>();
+    queue->push(var);
+    func->setSubExpr(queue);
+    func->setSubExprTree(std::make_shared<ExpressionNode>(var));
+    auto left = std::make_shared<ExpressionNode>(
+                                        std::make_shared<Number>("-1",-1));
+    auto right = std::make_shared<ExpressionNode>(func);
+    
+    testRoot->setLeft(left);
+    testRoot->setRight(right);
+    //std::cout << "\nout: " << LaTeXConverter::convertToLaTeX(testRoot) << "\n";
     return 0;
 }
