1cb7e077fac869e2cae5fb2175bb2b65a9e11ca0
Charlie Kushelevsky <ckushelevsky@ucsd.edu>
2024-10-19 18:32:02 -0700

    chore: fixed main function

--- src/main.cpp
+++ src/main.cpp
@@ -17,42 +17,18 @@ std::string getDerivative(std::string input)
 }
 int main(int argc, char const* argv[])
 {
-   /*std::cout << argc << "\n";
+   std::cout << argc << "\n";
     if (argc == 1)
     {
         std::cout << "Input needed\n";
         return 1;
     }
-    std::string input = argv[1];*/
+    std::string input = argv[1];
     
-    std::string input = "-2/5*ln(exp(x)-2*(2*x+3)/(5*x^2+x+4))+2/5*x";
-
+    
+    std::cout <<" \nInout: " << input << "\n";
     std::string out = getDerivative(input);
     std::cout <<" \nDerivative: " << out << "\n";
 
-
-    /*auto testRoot = std::make_shared<ExpressionNode>(
-                        std::make_shared<Operator>("+"));
-    
-    auto func = std::make_shared<Function>("sin");
-    func->setNegative(true);
-    auto var1 = std::make_shared<Variable>("x");
-    auto var2 = std::make_shared<Variable>("y");
-    auto queue = std::make_shared<TokenQueue>();
-    queue->push(var1);
-    func->setSubExpr(queue);
-    func->setSubExprTree(std::make_shared<ExpressionNode>(var1));
-    
-    auto left = std::make_shared<ExpressionNode>(var2);
-    auto right = std::make_shared<ExpressionNode>(func);
-    testRoot->setLeft(left);
-    testRoot->setRight(right);
-    TreeFixer::checkTree(testRoot);
-    std::cout << "\nout: " << LaTeXConverter::convertToLaTeX(testRoot) << "\n";
-
-
-    auto testNum = std::make_shared<Number>("1",1);
-    testNum->setNegative(true);
-    std::cout <<  "Equals: " << testNum->equals(-1) << "\n";*/
     return 0;
 }
