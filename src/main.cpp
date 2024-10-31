bd0f21ffe459c0d39973d1b00e646dce38e73dac
Charlie Kushelevsky <ckushelevsky@ucsd.edu>
2024-10-30 17:35:22 -0700

    ferat finsihed derivative handling and cli input

--- src/main.cpp
+++ src/main.cpp
@@ -1,34 +1,53 @@
 #include "derivative.hpp"
 #include "latex_converter.hpp"
 #include "token_queue.hpp"
-#include "calculator.hpp"
-
+#include "arithmetic.hpp"
+#include "log.hpp"
+#include "approx.hpp"
 
 
 #include <iostream>
 #include <string>
 #include <memory>
+#include <utility>
 
-std::string getDerivative(std::string input)
+#ifndef TEST_VERSION
+    #define TEST_VERSION 0
+#endif // !TEST_VERSION
+
+Logger getDerivative(std::string input)
 {
     
     Derivative out(input,"x");
-    return LaTeXConverter::convertToLaTeX(out.solve());
+    out.solve();
+    return out.log;
 }
 int main(int argc, char const* argv[])
 {
-   std::cout << argc << "\n";
-    if (argc == 1)
+    
+
+    // Set it to false
+    Arithmetic::floatSimplification = false;
+    
+    #if TEST_VERSION
+    std::string input = "ln(exp(x)-2*(2*x+3)/(5*x^2+x+4))";
+    
+    #else
+    if (argc < 3)
     {
         std::cout << "Input needed\n";
         return 1;
     }
     std::string input = argv[1];
+    #endif
     
     
-    std::cout <<" \nInout: " << input << "\n";
-    std::string out = getDerivative(input);
-    std::cout <<" \nDerivative: " << out << "\n";
-
+    Logger log = getDerivative(input);
+    Approx approximator(input,"x",9);
+    auto values = approximator.approximate();
+    std::cout << log.out() << "\n";
+    //std::cout << values.first << "\t" << values.second << "\n";
+    
+    
     return 0;
 }
